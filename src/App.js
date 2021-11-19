f29f9b3b3b7cd08b056af2b0208651ba9113228b
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-11-19 20:21:07 +0530

    feat: App.js subscriber added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-import React from 'react'
+import React ,{ useEffect} from 'react'
+import {Text} from 'react-native'
+import 'react-native-gesture-handler'
 
+import auth from '@react-native-firebase/auth'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
 
-const App =()=>{
+import { useDispatch , connect } from 'react-redux'
+
+import AddPost from './screens/AddPost'
+import Home from './screens/Home'
+import SignIn from './screens/SignIn'
+import SignUp from './screens/SignUp'
+import CustomHeader from './layout/CustomHeader'
+
+import {SET_POST,IS_AUTHENTICATED} from './action/action.types'
+import database from '@react-native-firebase/database'
+import EmptyContainer from './components/EmptyContainer'
+import {requestPermission} from './utils/AskPermission'
+
+const Stack =createStackNavigator();
+
+const App =({authstate})=>{
+
+    const dispatch=useDispatch();
+
+    const onAuthStateChanged=(user)=>{
+      
+      if(user){
+        
+          dispatch({
+            type:IS_AUTHENTICATED,
+            payload:true
+          })
+
+          console.log(user._user.uid);
+
+          database().ref(`/users/${user._user.uid}`)
+          .on('value',(snapshot)=>{
+            console.log('USER DETAILS',snapshot.val());
+            dispatch({
+              type:SET_POST,
+              payload:snapshot.val(),
+            })
+          })
+
+      }
+      else{
+          dispatch({
+            type:IS_AUTHENTICATED,
+            payload:false
+          })
+      }
+    }
+
+    useEffect(()=>{
+      requestPermission()
+      const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+      return subscriber();
+    },[])
+
     return(
       <>
+      <Text>App</Text>
       </>
     )
 }
+
 export default App;
