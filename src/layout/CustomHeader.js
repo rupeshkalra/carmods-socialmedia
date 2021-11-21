67febde8ff408f1d6ff0e55799efad41e8c32e11
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-11-21 15:29:44 +0530

    feat: CustomHeader connected to redux

--- src/layout/CustomHeader.js
+++ src/layout/CustomHeader.js
@@ -1,12 +1,62 @@
 import React from 'react'
-import {StyleSheet,Text} from 'react-native'
+import {StyleSheet} from 'react-native'
+import {
+    Header,
+    Body,
+    Right,
+    Button,
+    Icon,
+    Title,
+    Text
+} from 'native-base'
+import {connect} from 'react-redux'
+import {propTypes} from 'prop-types';
+import {signOut } from '../action/auth'
 
-const CustomHeader=()=>{
+const CustomHeader=({authState,SignOut,navigation})=>{
     return (
-        <>
-        <Text>custom header</Text>
-        </>
+      <Header
+      androidStatusBarColor="#0f4c75"
+      style={{
+          backgroundColor:"#0f4c75"
+      }}>
+          <Body>
+              <Title>CarSocial</Title>
+          </Body>
+          <Right>
+              {
+                  authState.isAuthenticated && (
+                      <>
+                      <Button
+                      transparent
+                      iconLeft
+                      onPress={()=>{navigation.navigate('AddPost')}}>
+                          <Text style={{color:"#fdcb9e"}}>AddPost</Text>
+                      </Button>
+                      <Button
+                      transparent
+                      onPress={()=>signOut()}>
+                          <Icon name="log-out-outline" style={{color:"red"}}/>
+                      </Button>
+                      </>
+                  )
+              }
+          </Right>
+      </Header>
     )
 }
 
-export default CustomHeader
+const mapStateToProps =(state)=>{
+    authState:auth.state
+}
+
+const mapDispatchToProps={
+    signOut
+}
+
+CustomHeader.propTypes={
+    signOut:propTypes.func.isRequired,
+    authState:propTypes.object.isRequired
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CustomHeader)
